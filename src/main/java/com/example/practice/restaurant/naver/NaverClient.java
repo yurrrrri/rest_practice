513ee4c4196cb9e0c624e3461d2f29c992d20a9b
yurison <yu_ri_son@naver.com>
2023-08-02 11:58:08 +0900

    지역검색 로직 구현

---
+++ src/main/java/com/example/practice/restaurant/naver/NaverClient.java
@@ -0,0 +1,55 @@
+package com.example.practice.restaurant.naver;
+
+import com.example.practice.restaurant.naver.dto.SearchLocalReq;
+import com.example.practice.restaurant.naver.dto.SearchLocalRes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+public class NaverClient {
+
+    @Value("${naver.client.id}")
+    private String naverClientId;
+
+    @Value("${naver.client.secret}")
+    private String naverClientSecret;
+
+    @Value("${naver.url.search.local}")
+    private String naverLocalSearchUrl;
+
+    @Value("${naver.url.search.image}")
+    private String naverImageSearchUrl;
+
+    public SearchLocalRes localSearch(SearchLocalReq req) {
+        var uri = UriComponentsBuilder.fromUriString(naverLocalSearchUrl)
+                .queryParams(req.toMultiValueMap())
+                .build()
+                .encode()
+                .toUri();
+
+        var headers = new HttpHeaders();
+        headers.set("X-Naver-Client-Id", naverClientId);
+        headers.set("X-Naver-Client-Secret", naverClientSecret);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        var httpEntity = new HttpEntity<>(headers);
+        var responseType = new ParameterizedTypeReference<SearchLocalRes>() {};
+
+        var responseEntity = new RestTemplate()
+                .exchange(uri, HttpMethod.GET, httpEntity, responseType);
+
+        return responseEntity.getBody();
+    }
+
+    public void imageSearch() {
+
+    }
+
+}
